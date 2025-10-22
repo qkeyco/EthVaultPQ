46429661313dc11d94cfc947c584a1dab8bf5458
jamestagg <james@taggs.com>
2025-10-22 13:41:40 -0700

    dashboard integrate

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { NotificationProvider, TransactionPopupProvider } from '@blockscout/app-sdk';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NotificationProvider>
-      <TransactionPopupProvider>
-        <App />
-      </TransactionPopupProvider>
-    </NotificationProvider>
+    <App />
   </React.StrictMode>,
 );
